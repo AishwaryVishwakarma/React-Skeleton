9884493f40088ec20a4602ca14b0b3d9f6997b02
Aishwary Vishwakarma <aishwaryvishwakarma23@gmail.com>
2024-02-10 04:09:10 +0530

    fix: build fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
-import {terser} from 'rollup-plugin-terser';
 
 const packageJson = require('./package.json');
 
